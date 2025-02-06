13881805f1587ff18d7bb13a3f51571dfc872143
kainwang <rogerboukar07@gmail.com>
2025-02-06 19:54:30 +0100

    configuration du fichier deploy.sh

--- Makefile
+++ Makefile
@@ -11,11 +11,11 @@ install:
 
 build:
 	@echo "Building the project..."
-	docker build -t rogerboukar07/allmight_bot .
+	docker build -t rogerboukar07/kainwang_bot .
 
 make deploy:
 	@echo "Deploying the project..."
-	docker run -d -p 8007:80 rogerboukar07/allmight_bot
+	docker run -d -p 8007:80 rogerboukar07/kainwang_bot
 
 serve:
 	@echo "Starting development server..."
--- deploy.sh
+++ deploy.sh
@@ -7,22 +7,24 @@ then
     exit 1
 fi
 
+CONTAINER_NAME=$1
+
 # checks if container exist
-if [ "$(docker ps -a -q -f name=$1)" ]
+if [ "$(docker ps -a -q -f name=$CONTAINER_NAME)" ]
 then
-    echo "An existing container with the name $1 was found!"
+    echo "An existing container with the name $CONTAINER_NAME was found!"
 
     # checks if container is running and stop it if it is
-    if [ "$(docker ps -aq -f status=running -f name=$1)" ]
+    if [ "$(docker ps -aq -f status=running -f name=$CONTAINER_NAME)" ]
     then
         echo "Stopping container..."
-        docker stop $1
+        docker stop $CONTAINER_NAME
 	echo "Container stopped."
     fi
 
     # removes stopped container
     echo "Removing stopped container..."
-    docker rm -f $1
+    docker rm -f $CONTAINER_NAME
     echo "Container removed."
 fi
 
@@ -30,4 +32,4 @@ fi
 docker pull repo/image:master
 
 # run new docker container
-docker run -d --restart always --name $1 --env-file ./.env hervlokossou/allmight_bot:master
+docker run -d --restart always --name $CONTAINER_NAME --env-file ./.env rogerboukar07/kainwang_bot:master
